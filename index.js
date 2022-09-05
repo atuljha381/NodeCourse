aa6ca76ce22a90581668438c306cbbd985e0ba7f
Atul Jha <mail.atuljha@gmail.com>
2022-09-05 13:42:18 +0530

    Adding Logger.js and changes to index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const person = require('./person')
+// const person = require('./person')
 
-const person1 = new person('Atul Jha',22)
-person1.greetings()
+// const person1 = new person('Atul Jha',22)
+// person1.greetings()
+
+const Logger = require("./logger");
+const logger = new Logger();
+
+logger.on("message", (data) => console.log("Called Listener:", data));
+
+logger.log("Hello World!");
